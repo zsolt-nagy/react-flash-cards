9b8445d14563f74f983a1819629f43f3effdd43c
Zsolt Nagy <info@zsoltnagy.eu>
2020-05-01 18:50:37 +0200

    Controlled components

--- src/components/CardsForm/CardsForm.js
+++ src/components/CardsForm/CardsForm.js
@@ -2,20 +2,35 @@ import React from 'react';
 import './CardsForm.css';
 
 function CardsForm(props) {
+    const [frontSide, setFrontSide] = React.useState('');
+    const [backSide, setBackSide] = React.useState('');
+    function handleFrontSideChange(e) {
+        setFrontSide(e.target.value);
+    }
+    function handleBackSideChange(e) {
+        setBackSide(e.target.value);
+    }
     function handleSubmit(e) {
-        e.preventDefault();
+        e.preventDefault(); 
+        alert( `${frontSide} ${backSide}` );
     }
     return (
         <form action="#" onSubmit={handleSubmit} className="form-container">
             <label className="textfield-container">
                 Front side:
-                <input type="text" 
+                <input type="text"  
+                       onChange={handleFrontSideChange}
+                       value={frontSide}
+                       required
                        placeholder="Front side" 
                        className="js-front-side textfield-input" />
             </label>
             <label className="textfield-container">
                 Back side:
                 <input type="text" 
+                       onChange={handleBackSideChange}
+                       value={backSide}
+                       required
                        placeholder="Back side" 
                        className="js-back-side textfield-input" />
             </label>
